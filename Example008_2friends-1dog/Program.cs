56419efef25c2a1a65c769b24b517ac8ef7c8585
akunts <david@akunts.ru>
2022-10-12 17:29:50 +0400

    task about frinds and dog final

--- Example008_2friends-1dog/Program.cs
+++ Example008_2friends-1dog/Program.cs
@@ -1,30 +1,32 @@
-﻿Console.Clear();
-int count = 0;
-int distance = 30000;
-int firstFriendSpeed = 1;
-int secondFriendSpeed = 2;
-int dogSpeed = 5;
-int friend = 2;
-int time = 0;
+﻿Console.Clear(); // Очищаем терминал
+int count = 0; // Задаем счетчик пробежек собаки
+Console.WriteLine("Введите расстояние (м)"); // Спрашиваем расстояние
+int distance = Convert.ToInt32(Console.ReadLine()); // Получаем расстояние
+Console.WriteLine("Введите скорость 1-го друга (м/с)"); // Спрашиваем скорость 1-го друга
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine()); // Получаем скорость 1-го друга
+Console.WriteLine("Введите скорость 2-го друга (м/с)"); // Спрашиваем скорость 2-го друга
+int secondFriendSpeed = Convert.ToInt32(Console.ReadLine()); // Получаем скорость 2-го друга
+Console.WriteLine("Введите скорость собаки (м/с)"); // Спрашиваем скорость собаки
+int dogSpeed = Convert.ToInt32(Console.ReadLine()); // Получаем скорость собаки
+int friend = 2; // Переменная для указания направления бег собаки (от 1 ко 2 или наоборот)
 
-while(distance>10)
+while(distance>10) // цикл будет идти до тех пор, пока расстояние между друзьями не уменьшится до 10 метров
 {
-    if(friend == 2)
+    if(friend == 2) // вычисления, если собака бежит от 1 друга ко 2
     {
-        time = distance / (secondFriendSpeed + dogSpeed);
-        friend = 1;
-        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
-        count++;
+        int time = distance / (secondFriendSpeed + dogSpeed); // вычисляем время
+        friend = 1; // меняем переменную, чтобы сообщить, что направление собаки поменяется
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time; // корректируем дистанцию между друзьями
+        count++; // увеличивем счетчик пробежек на 1
     }
-    else
+    else // вычисления, если собака бежит от 2 друга к 1
     {
-        time = distance / (firstFriendSpeed + dogSpeed);
-        friend = 2;
-        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
-        count++;
+        int time = distance / (firstFriendSpeed + dogSpeed); // вычисляем время
+        friend = 2; // меняем направление собаки
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time; // корректируем дистанцию
+        count++; // увеличиваем счетчик пробежек
     }
 }
 
-Console.Write("Собака пробежит ");
-Console.Write(count);
-Console.Write(" раз");
+Console.WriteLine("Собака пробежит " + count + " раз"); // выводим итоговое количество пробежек
+
